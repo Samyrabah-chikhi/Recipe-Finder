ca04502b3138761e50a7437403e21d4d739dad8e
CHIKHI Samy Rabah <csamyrabah@gmail.com>
2025-06-07 03:29:03 +0200

    Reactive food cards

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FoodCard({ title, image }) {
   return (
-    <div className="bg-red-300 p-2 lg:w-1/4 md:w-1/4 sd:w-1/6  hover:scale-[1.02] transition duration-500">
+    <div className="bg-red-300 p-2 lg:w-1/4 md:w-1/3 sd:w-1/6 rounded-lg hover:scale-[1.02] transition duration-500">
       <div className="flex justify-between">
         <h3 className="text-amber-800">{title}</h3> 
         <button className="hover:scale-110 duration-300">❤️</button>
